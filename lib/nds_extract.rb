7e0669453cbf4ad92b0b64fffda282a8024a7699
David Holmes <phoenix1988@gmail.com>
2019-12-27 21:50:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,8 +2,26 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  require 'pp'
-  pp directors_database 
+  total_spielberg = 0 
+  spielbergarray = 0
+  while spielbergarray < directors_database[0][:movies].length do 
+    total_spielberg += directors_database[0][:movies][:worldwide_gross]
+    array2 += 1
+  end 
+  total_russo = 0
+  russoarray = 0
+  while russoarray < directors_database[1][:movies].length do 
+    total_russo += directors_database[1][:movies][:worldwide_gross]
+    russoarray += 1
+  end 
+
+
+  money_hash = {
+    "Steven Spielberg" => total_spielberg,
+    "Russo Brothers" => total_russo
+  }
+  
+  
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #

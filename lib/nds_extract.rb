4ce1480c87d96b84bbed52ebe8ffe7177ed67157
David Holmes <phoenix1988@gmail.com>
2019-12-27 21:50:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,7 @@ def directors_totals(nds)
     "Russo Brothers" => total_russo
   }
 
-  return money_hash 
+  return money_hash
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
